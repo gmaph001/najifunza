cebbc4246ed839434fc22f778b06c9ffd094b3b5
GEORGE MAPHOLE <gmaph001@gmail.com>
2025-05-20 11:42:38 +0300

    Security updates

--- database/najifunza.sql
+++ database/najifunza.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.2
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: May 12, 2025 at 07:45 AM
--- Server version: 10.6.21-MariaDB-cll-lve-log
--- PHP Version: 8.3.19
+-- Host: 127.0.0.1
+-- Generation Time: May 20, 2025 at 10:40 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `vimerkba_najifunza`
+-- Database: `najifunza`
 --
 
 -- --------------------------------------------------------
@@ -36,7 +36,7 @@ CREATE TABLE `admin` (
   `phone` varchar(10) NOT NULL,
   `username` varchar(500) NOT NULL,
   `email` varchar(700) NOT NULL,
-  `password` varchar(100) NOT NULL,
+  `password` varchar(1000) NOT NULL,
   `rank` int(5) NOT NULL,
   `codename` varchar(20) NOT NULL,
   `userkey` int(9) NOT NULL,
@@ -50,11 +50,7 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`admin_ID`, `firstname`, `secondname`, `lastname`, `school`, `phone`, `username`, `email`, `password`, `rank`, `codename`, `userkey`, `OTP`, `photo`, `security`) VALUES
-(2, 'FUMBUKA', 'LIMBU', 'MATALU', 'SHAABAN ROBERT SEC SCHOOL', '0765334612', 'MR LIMBU', 'limbufumbuka@gmail.com', 'fumbuka@1998', 1, 'TEA', 194384423, 148511, 'media/images/prof_pics/login.png', '41.59.194.176'),
-(3, 'Edmund', 'E', 'Eberhard', 'Shaaban Robert Secondary School', '0656144795', 'edmund.eberhard', 'najifunza@gmail.com', 'hapakazitu', 1, 'TEA', 895375998, 202260, 'media/images/prof_pics/login.png', 'out'),
-(4, 'Karishma', 'Punit', 'Chhatbar', 'Shaaban Robert Secondary School', '0745520501', 'karishma', 'karishmachhatbartz@gmail.com', '123456', 1, 'TEA', 554156717, 816474, 'media/images/prof_pics/login.png', '41.59.194.176'),
-(5, 'ramadhani', 'said', 'chimbanga', 'shaaban robert secondary school', '0718010540', 'chimbanga', 'chimbanga2011@gmail.com', 'chimbanga', 1, 'TEA', 843879395, 886467, 'media/images/prof_pics/login.png', '41.59.194.176'),
-(9, 'George', 'Godson', 'Maphole', 'Shaaban Robert Secondary School', '0626303582', 'gmaph__001', 'gmaph001@gmail.com', '2212241718', 404, 'ADM', 403991094, 555674, 'media/images/prof_pics/wallpaperflare.com_wallpaper (3).jpg', 'out');
+(7, 'George', 'Godson', 'Maphole', 'Shaaban Robert Secondary School', '0748554514', 'gmaph__001', 'gmaph001@gmail.com', '$2y$10$j5EdUu6pgo9IdoF5EzVKa.P3AJ8B0qEmns9Gl1KKaWJTaaCvr11X.', 404, 'ADM', 696921611, 986204, 'media/images/prof_pics/wallpaperflare.com_wallpaper (3).jpg', '::1');
 
 -- --------------------------------------------------------
 
@@ -80,10 +76,39 @@ CREATE TABLE `assignments` (
 
 CREATE TABLE `classes` (
   `class_ID` int(11) NOT NULL,
+  `creator` int(9) NOT NULL,
   `class_name` varchar(500) NOT NULL,
-  `password` varchar(9) NOT NULL,
+  `password` varchar(500) NOT NULL,
+  `class_photo` varchar(1000) NOT NULL,
   `class_key` int(9) NOT NULL,
-  `no_users` int(5) NOT NULL
+  `no_users` int(100) NOT NULL,
+  `create_date` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `classes`
+--
+
+INSERT INTO `classes` (`class_ID`, `creator`, `class_name`, `password`, `class_photo`, `class_key`, `no_users`, `create_date`) VALUES
+(3, 696921611, 'Gmaph Class', '$2y$10$1oU4Cl0ZhTfwW4B2yQ22Iexr1fxxLxePVkFMN4NMeAobop0HMYucm', 'media/classes/images/584055864.jpg', 584055864, 0, '2025-05-19'),
+(5, 696921611, 'Anonymous', '$2y$10$GE46r0OERTxZU61tUtkTgORFbdcygd8O8X1PYcpvM4EU/c2HVMO76', 'media/classes/images/235573918.jpg', 235573918, 0, '2025-05-19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `class_activity`
+--
+
+CREATE TABLE `class_activity` (
+  `act_ID` int(11) NOT NULL,
+  `class_key` int(9) NOT NULL,
+  `userkey` int(9) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `mat_name` varchar(500) NOT NULL,
+  `description` text NOT NULL,
+  `mat_key` int(9) NOT NULL,
+  `time` time NOT NULL,
+  `date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -154,16 +179,15 @@ CREATE TABLE `notes` (
 --
 
 INSERT INTO `notes` (`notes_ID`, `poster`, `poster_ID`, `subject`, `notes`, `level`, `category`, `class`, `description`, `notes_key`) VALUES
-(3, 'mr  limbu', 194384423, 'COMPUTER SCIENCE', 'media/documents/notes/form 01 notes 2.pdf', '2', 'secular', 1, 'introduction to computer science', 111264329),
-(4, 'mr limbu', 194384423, 'COMPUTER SCIENCE', 'media/documents/notes/forn two 2025.pdf', '2', 'secular', 2, 'word & excel notes', 965980282),
-(5, 'MR LIMBU', 194384423, 'COMPUTER SCIENCE', 'media/documents/notes/COMPUTER SCIENCE FOR ORDINARY LEVEL_FORM 1.pdf', '2', 'secular', 1, 'notes for chapter 1 - 3', 529385658),
-(6, 'MR EDMUND', 554156717, 'COMPUTER SCIENCE', 'media/documents/notes/Computer System & Hardware.pdf', '2', 'secular', 1, 'Form 1 Notes', 601783952),
-(7, 'Mr. Edmund', 895375998, 'COMPUTER SCIENCE', 'media/documents/notes/Computer Science Notes 2025.pdf', '2', 'secular', 1, 'Form 1 Notes', 789986120),
-(8, 'chimbanga', 843879395, 'HISTORY', 'media/documents/notes/HISTORY FORM 3-2025.pptx', '2', 'secular', 3, 'write the notes given in your exercise book', 488032186),
-(9, 'chimbanga', 843879395, 'GEOGRAPHY', 'media/documents/notes/Theories of the shape of the Earth.pptx', '2', 'secular', 5, 'write the notes  given in your exercise book', 662508023),
-(10, 'Mr. Edmund', 895375998, 'COMPUTER SCIENCE', 'media/documents/notes/2. WEB DEVELOPMENT PDF.pdf', '2', 'secular', 3, 'FORM THREE WEB DEVELOPMENT', 358085241),
-(13, 'Mr. Edmund', 895375998, 'COMPUTER SCIENCE', 'media/documents/notes/2. SPREAD SHEET FINAL PDF.pdf', '2', 'secular', 2, 'FORM TWO- SPREADSHEET NOTES', 311772636),
-(14, 'Mr. Edmund', 895375998, 'COMPUTER SCIENCE', 'media/documents/notes/COMPUTER HARDWARE.pdf', '2', 'secular', 1, 'COMPUTER HARDWARE NOTES', 694707495);
+(3, 'MR LIMBU', 215441274, 'COMPUTER SCIENCE', 'media/documents/notes/C++ notes.pdf', '2', 'secular', 5, 'C++ Notes\' for Form 5', 745666954),
+(5, 'MR SALIM', 215441274, 'PHYSICS', 'media/documents/notes/Class Notes - Surface tension.pdf', '2', 'secular', 5, 'Surface Tension notes', 733063990),
+(7, 'MR JOANES', 215441274, 'BIOLOGY', 'media/documents/notes/Biology f 3 Study notes.pdf', '2', 'secular', 3, 'Biology notes for Form 3. All chapters.', 370960529),
+(9, 'MR GEORGE', 215441274, 'GEOGRAPHY', 'media/documents/notes/MY PAMPHLATE GEOGRAPHY.pdf', '2', 'secular', 3, 'Geography notes for Form 3.', 998396969),
+(11, 'MR JOANES', 215441274, 'BIOLOGY', 'media/documents/notes/CELL STRUCTURE AND ORGANIZATION O-Level(1).docx', '2', 'secular', 1, 'Cell structure and organization.', 518480222),
+(16, 'MR SALIM', 215441274, 'PHYSICS', 'media/documents/notes/CIRCULAR MOTION.pdf', '2', 'secular', 5, 'Circular Motion', 619390235),
+(19, 'SYS-ADMIN', 516708124, 'COMPUTER SCIENCE', 'media/documents/notes/notes_136406560', '2', 'secular', 5, 'Web Development.', 136406560),
+(21, 'SYS-ADMIN', 516708124, 'COMPUTER SCIENCE', 'media/documents/notes/notes_301307611', '2', 'secular', 6, 'Data Structures and Algorithms', 301307611),
+(22, 'SYS-ADMIN', 696921611, 'COMPUTER SCIENCE', 'media/documents/notes/notes_565682642', '2', 'secular', 6, 'Networking', 565682642);
 
 -- --------------------------------------------------------
 
@@ -178,20 +202,6 @@ CREATE TABLE `saved` (
   `saved_key` int(9) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `saved`
---
-
-INSERT INTO `saved` (`act_ID`, `category`, `saver`, `saved_key`) VALUES
-(3, 'notes', 759461116, 111264329),
-(4, 'notes', 194384423, 111264329),
-(8, 'notes', 759461116, 601783952),
-(9, 'notes', 759461116, 529385658),
-(15, 'notes', 403991094, 694707495),
-(16, 'notes', 403991094, 311772636),
-(17, 'notes', 403991094, 662508023),
-(18, 'notes', 403991094, 488032186);
-
 -- --------------------------------------------------------
 
 --
@@ -202,8 +212,9 @@ CREATE TABLE `users` (
   `user_ID` int(11) NOT NULL,
   `username` varchar(200) NOT NULL,
   `email` varchar(700) NOT NULL,
-  `password` varchar(100) NOT NULL,
+  `password` varchar(1000) NOT NULL,
   `userkey` int(9) NOT NULL,
+  `OTP` int(6) NOT NULL,
   `photo` varchar(500) NOT NULL,
   `security` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -212,21 +223,9 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_ID`, `username`, `email`, `password`, `userkey`, `photo`, `security`) VALUES
-(2, 'MR LIMBU', 'limbufumbuka@gmail.com', 'fumbuka@1998', 194384423, 'media/images/prof_pics/login.png', '41.59.194.176'),
-(3, 'edmund.eberhard', 'najifunza@gmail.com', 'hapakazitu', 895375998, 'media/images/prof_pics/login.png', 'out'),
-(4, 'karishma', 'karishmachhatbartz@gmail.com', '123456', 554156717, 'media/images/prof_pics/login.png', '41.59.194.176'),
-(5, 'chimbanga', 'chimbanga2011@gmail.com', 'chimbanga', 843879395, 'media/images/prof_pics/login.png', '41.59.194.176'),
-(6, 'Salha', 'salha.islam@gmail.com', 'shangazi', 714761398, 'media/images/prof_pics/login.png', '41.222.181.72'),
-(7, 'processor', 'gmaph001@gmail.com', '123456', 597694546, 'media/images/prof_pics/login.png', 'out'),
-(8, 'Ma', 'ffahima@gmail.com', '23', 327295246, 'media/images/prof_pics/login.png', '41.59.234.32'),
-(9, 'Eirean Apiyo', 'daileirean21@gmail.com', 'monalisaapiyo', 562600278, 'media/images/prof_pics/login.png', '41.207.242.18'),
-(10, 'Henry JR', 'henryrusasa@gmail.com', 'Nakatunga,5', 214086831, 'media/images/prof_pics/login.png', '41.59.194.176'),
-(11, 'Salha Suleyman Hassan', 'ismailmwamtoro@gmail.com', 'rolha0911', 121107086, 'media/images/prof_pics/login.png', '102.211.34.222'),
-(12, 'nick_john', 'nickisjohn@gmail.com', '10651206512a', 155010518, 'media/images/prof_pics/login.png', '41.59.194.176'),
-(13, 'gmaph__001', 'gmaph001@gmail.com', '2212241718', 403991094, 'media/images/prof_pics/wallpaperflare.com_wallpaper (3).jpg', 'out'),
-(14, 'SULEIMAN HASSAN', 'suleimanommar11@gmail.com', 'srss123srss', 509838249, 'media/images/prof_pics/login.png', '41.59.194.176'),
-(15, '56u', 'mohammedsk5318@gmail.com', '675', 603192235, 'media/images/prof_pics/login.png', '41.59.194.176');
+INSERT INTO `users` (`user_ID`, `username`, `email`, `password`, `userkey`, `OTP`, `photo`, `security`) VALUES
+(8, 'gmaph__001', 'gmaph001@gmail.com', '$2y$10$j5EdUu6pgo9IdoF5EzVKa.P3AJ8B0qEmns9Gl1KKaWJTaaCvr11X.', 696921611, 784824, 'media/images/prof_pics/wallpaperflare.com_wallpaper (3).jpg', '::1'),
+(9, '@muhy', 'moshaagnes1968@gmail.com', '$2y$10$MZeyBs7N.dIvdh7hTosYpuai4rKHmBs3vMbe2bzMPXjpj2/Y8cHtq', 974310072, 0, 'media/images/prof_pics/wallpaperflare.com_wallpaper (25).jpg', 'out');
 
 --
 -- Indexes for dumped tables
@@ -249,6 +248,12 @@ ALTER TABLE `assignments`
 --
 ALTER TABLE `classes`
   ADD PRIMARY KEY (`class_ID`);
+
+--
+-- Indexes for table `class_activity`
+--
+ALTER TABLE `class_activity`
+  ADD PRIMARY KEY (`act_ID`);
 
 --
 -- Indexes for table `exams`
@@ -294,7 +299,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `admin_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `admin_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `assignments`
@@ -306,7 +311,13 @@ ALTER TABLE `assignments`
 -- AUTO_INCREMENT for table `classes`
 --
 ALTER TABLE `classes`
-  MODIFY `class_ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `class_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `class_activity`
+--
+ALTER TABLE `class_activity`
+  MODIFY `act_ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `exams`
@@ -330,19 +341,19 @@ ALTER TABLE `news`
 -- AUTO_INCREMENT for table `notes`
 --
 ALTER TABLE `notes`
-  MODIFY `notes_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `notes_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `saved`
 --
 ALTER TABLE `saved`
-  MODIFY `act_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `act_ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `user_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
